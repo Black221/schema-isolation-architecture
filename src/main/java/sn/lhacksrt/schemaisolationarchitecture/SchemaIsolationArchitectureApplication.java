f482a49052e25d88aed06973299407d13c21299c
LHackSRT <bambabar02@gmail.com>
2024-12-05 08:52:08 +0000

    initialisation

--- src/main/java/sn/lhacksrt/schemaisolationarchitecture/SchemaIsolationArchitectureApplication.java
+++ src/main/java/sn/lhacksrt/schemaisolationarchitecture/SchemaIsolationArchitectureApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SchemaIsolationArchitectureApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchemaIsolationArchitectureApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SchemaIsolationArchitectureApplication.class, args);
+    }
 
 }
