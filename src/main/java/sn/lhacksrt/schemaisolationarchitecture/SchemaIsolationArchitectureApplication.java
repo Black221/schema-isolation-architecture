bc9b0df417de7ff1acba0ea1a6f55c4bd322fd24
LHackSRT <bambabar02@gmail.com>
2024-12-05 08:51:51 +0000

    first commit

---
+++ src/main/java/sn/lhacksrt/schemaisolationarchitecture/SchemaIsolationArchitectureApplication.java
@@ -0,0 +1,13 @@
+package sn.lhacksrt.schemaisolationarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaIsolationArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaIsolationArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/lhacksrt/schemaisolationarchitecture/SchemaIsolationArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.lhacksrt.schemaisolationarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaIsolationArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
